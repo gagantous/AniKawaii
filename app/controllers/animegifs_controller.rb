b7c4de22da5a5df4c5ec76cce15615c75aef41b0
wraithseeker <wraithseekers@gmail.com>
2015-10-15 18:10:32 +0800

    show gif page added

--- app/controllers/animegifs_controller.rb
+++ app/controllers/animegifs_controller.rb
@@ -1,9 +1,23 @@
 class AnimegifsController < ApplicationController
+
+   
+
+  
 	def new
 		@Animegif = Animegif.new
 	end
+	def index
+
+	end
+	def show
+		@Animegif = Animegif.find(params[:id])
+
+	end
 
 	def create
-
+		@Animegif = User.new
+		if @Animegif.save
+			redirect_to @user
+		end
 	end
 end
