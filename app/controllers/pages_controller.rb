d590bc0ee01e526fca7440143e8e39d3afc92c78
wraithseeker <wraithseekers@gmail.com>
2015-10-22 13:23:03 +0800

    update votes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
 	
 	def index
 		#For some reason per_page is -2 the actual number
-		@animegif = Animegif.paginate(:page => params[:page], :per_page => 12).order('created_at DESC')
+		@animegif = Animegif.paginate(:page => params[:page], :per_page => 12).order('created_at ASC')
 
 		##Post.page(params[:page]).order('created_at DESC')	
 
