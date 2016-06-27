409a9bf7a85541b98813ac010320f0df5879aba1
wraithseeker <wraithseekers@gmail.com>
2016-06-27 21:32:58 +0800

    clean up some old code, use SSL HTTPS

---
+++ app/assets/javascripts/gif.js
@@ -0,0 +1,15 @@
+if((navigator.userAgent.match(/iPad/i)) && (navigator.userAgent.match(/iPad/i)!= null)){
+  		
+		$('video').prop("controls",true);
+	}
+
+
+
+ (function(d, t) {
+    var g = d.createElement(t),
+        s = d.getElementsByTagName(t)[0];
+    g.src = 'http://assets.gfycat.com/js/gfyajax-0.517d.js';
+    s.parentNode.insertBefore(g, s);
+}(document, 'script'));
+
+
